29a1767cb7b61bee2a03869ecc8865e338adaeb0
ricky-kiva <85386584+ricky-kiva@users.noreply.github.com>
2026-09-16 08:49:29 -0400

    implemented delete account

--- service/routes.py
+++ service/routes.py
@@ -113,6 +113,14 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    account = Account.find(account_id)
+
+    if account:
+        account.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
